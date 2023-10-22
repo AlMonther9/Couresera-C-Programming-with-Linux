c5261deb12f919711499c710549e528e4eae26ee
AlMonther9 <139255318+AlMonther9@users.noreply.github.com>
2023-10-22 12:46:08 +0300

    Update perform simple arithmetic in C.c

--- perform simple arithmetic in C.c
+++ perform simple arithmetic in C.c
@@ -8,9 +8,12 @@ You still have to wait for X days (Y minutes or Z seconds) before you can procra
 // The code:
 #include <stdio.h>
 int main(void){
-    int X = (25-23);
-    int Y = (60*24*X);
-    int Z = (60*Y);
+    int X;
+    int Y;
+    int Z;
+    X = (25-23);
+    Y = (60*24*X);
+    Z = (60*Y);
     printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!\n",X ,Y ,Z);
     return 0;
 }
